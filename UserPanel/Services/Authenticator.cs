765c981e292b6abef4d166c83826f82aa7d2d9da
zogov123 <danielkrich147@gmail.com>
2022-02-03 17:22:47 +0200

    Updates

--- UserPanel/Services/Authenticator.cs
+++ UserPanel/Services/Authenticator.cs
@@ -13,6 +13,7 @@ namespace UserPanel.Services
 {
     public class Authenticator : IAuthenticator
     {
+        private static string LoginUrl { get; } = "http://127.0.0.1/api/login";
         public UserModel User { get; set; }
         public bool Authorized { get; set; }
 
@@ -23,7 +24,7 @@ namespace UserPanel.Services
                 Username = username,
                 Password = password
             }), Encoding.UTF8, "application/json");
-            HttpResponseMessage res = request.PostAsync("http://127.0.0.1/api/login", payload).Result;
+            HttpResponseMessage res = request.PostAsync(LoginUrl, payload).Result;
             User = JsonConvert.DeserializeObject<UserModel>(res.Content.ReadAsStringAsync().Result);
             Authorized = User.Username == username && username.Length > 0 ? true : false;
             if (!Authorized)
